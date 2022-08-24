e69a4e38888e0af1941d8f0ac1ab6b60f2ef19c0
lucasbarsaglini <lucasbarsag@gmail.com>
2022-08-23 21:50:36 -0300

    User entity and resource

---
+++ src/main/java/com/example/web/entities/User.java
@@ -0,0 +1,30 @@
+package com.example.web.entities;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@Data
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
+	
+	public User() {
+	}
+
+	public User(Long id, String name, String email, String phone, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+
+	
+}
