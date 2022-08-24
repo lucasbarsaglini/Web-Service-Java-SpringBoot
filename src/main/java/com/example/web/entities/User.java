a6528587e5eb7d0c0d7d921ed1d752c0bfa11c12
lucasbarsaglini <lucasbarsag@gmail.com>
2022-08-23 22:20:58 -0300

    H2 database, test profile, JPA

--- src/main/java/com/example/web/entities/User.java
+++ src/main/java/com/example/web/entities/User.java
@@ -2,12 +2,22 @@ package com.example.web.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -25,6 +35,4 @@ public class User implements Serializable {
 		this.phone = phone;
 		this.password = password;
 	}
-
-	
 }
